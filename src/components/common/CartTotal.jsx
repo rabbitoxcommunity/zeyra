bc778e569b2b238e089151e1c7dc0b08be8fab63
aslam <aslam@smarthatch.com>
2026-01-07 07:44:34 +0530

    removed unused files

--- src/components/common/CartTotal.jsx
+++
@@ -1,8 +0,0 @@
-"use client";
-
-import { useContextElement } from "@/context/Context";
-
-export default function CartTotal() {
-  const { totalPrice } = useContextElement();
-  return <>{totalPrice.toFixed(2)}</>;
-}
